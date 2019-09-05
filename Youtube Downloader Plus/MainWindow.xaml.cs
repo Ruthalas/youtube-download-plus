d73db98350b48f1517d0d5b30a1f76b52494f688
Ruthalas <ruthalas.bounce@gmail.com>
2019-09-04 20:25:57 -0700

    Updated MainWindow
    Added basic running of command line and capture of output.

--- Youtube Downloader Plus/MainWindow.xaml.cs
+++ Youtube Downloader Plus/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,44 @@ namespace Youtube_Downloader_Plus
         public MainWindow()
         {
             InitializeComponent();
+
+            string strCommand = "W:\\VIDEO\\OTHER\\YOUTUBE Staging\\youtube-dl.exe";
+            string strCommandParameters = "--version";
+            string strWorkingDirectory = "W:\\VIDEO\\OTHER\\YOUTUBE Staging";
+
+            //Create process
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+
+            //strCommand is path and file name of command to run
+            pProcess.StartInfo.FileName = strCommand;
+
+            //strCommandParameters are parameters to pass to program
+            pProcess.StartInfo.Arguments = strCommandParameters;
+
+            pProcess.StartInfo.UseShellExecute = false;
+
+            //Set output of program to be written to process output stream
+            pProcess.StartInfo.RedirectStandardOutput = true;
+
+            //Optional
+            pProcess.StartInfo.WorkingDirectory = strWorkingDirectory;
+
+            //Start the process
+            pProcess.Start();
+
+            //Get program output
+            string strOutput = pProcess.StandardOutput.ReadToEnd();
+            textBlock2.Text = textBlock2.Text + strOutput;
+
+            //Wait for process to finish
+            pProcess.WaitForExit();
+
         }
 
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
